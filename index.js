f879596e40de07bdd267ee95ee33aa7bacb3def3
aditi <aditideshpande2908@gmail.com>
2025-08-13 20:35:20 +0530

    add backend code

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(cors())
+
+let notes = [
+	{
+		id: '1',
+		content: 'HTML is easy',
+		important: true,
+	},
+	{
+		id: '2',
+		content: 'Browser can execute only JavaScript',
+		important: false,
+	},
+	{
+		id: '3',
+		content: 'GET and POST are the most important methods of HTTP protocol',
+		important: true,
+	},
+]
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+	res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+	const id = req.params.id
+	const note = notes.find((note) => note.id === id)
+
+	if (!note) res.status(404).end('no data present')
+	else res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+	const id = req.params.id
+	notes = notes.filter((note) => note.id !== id)
+
+	res.status(204).end('deleted')
+})
+
+const generateId = () => {
+	// recall: how is this logic working?
+	const maxId =
+		notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
+	return String(maxId + 1)
+}
+
+app.post('/api/notes', (req, res) => {
+	const body = req.body
+	if (!body.content) {
+		return res.status(400).json({ error: 'content missing' })
+	}
+
+	const note = {
+		content: body.content,
+		important: body.important || false,
+		id: generateId(),
+	}
+
+	notes.push(note)
+	res.json(note)
+})
+
+const requestLogger = (req, res, next) => {
+	console.log('method', req.method)
+	console.log('Path:  ', req.path)
+	console.log('Body:  ', req.body)
+	next()
+}
+
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.listen(PORT, () => {
+	console.log(`server running on port `, PORT)
+})
