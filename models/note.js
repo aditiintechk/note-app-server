dc633a2fb94bfd0aacaedc13e79ed32b7a83f87d
aditi <aditideshpande2908@gmail.com>
2025-08-14 21:04:29 +0530

    structure the backend

--- models/note.js
+++ models/note.js
@@ -1,26 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-const url = process.env.MONGO_URI
-mongoose
-	.connect(url)
-	.then((result) => {
-		console.log('connected to mongodb')
-	})
-	.catch((error) => {
-		console.log('error connecting to mongodb', error.message)
-	})
-
 const noteSchema = new mongoose.Schema({
 	content: {
 		type: String,
-		minLength: 5,
 		required: true,
+		minlength: 5,
 	},
 	important: Boolean,
 })
 
-// format the response
 noteSchema.set('toJSON', {
 	transform: (document, returnedObject) => {
 		returnedObject.id = returnedObject._id.toString()
