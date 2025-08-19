fd2d75097f508bcc40b7e82c3a1c2a7b790170c0
aditi <aditideshpande2908@gmail.com>
2025-08-19 20:42:54 +0530

    token based auth

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger.js')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes.js')
 const usersRouter = require('./controllers/users.js')
+const loginRouter = require('./controllers/login.js')
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
